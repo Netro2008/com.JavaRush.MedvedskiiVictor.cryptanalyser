e0c2dc42f660e3207c06fbae0472edc979413a77
Victor Medvedskii <[akulka2022@gmail.com]>
2025-02-11 21:58:10 +0300

    Fixed the code!

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -3,20 +3,18 @@ import java.io.IOException;
 import java.util.List;
 
 public class BruteForce {
-    public static int indexesOfFiles3;
-    public static int index3;
+    public static int indexesOfFiles;
     public static String bruteForce = "";
     public static String bruteForce2 = "";
 
     public static void decryptByBruteForceFiles(String filepath, List<Character> alphabet) {
-        index3 = 1;
-        indexesOfFiles3 = 1;
+        indexesOfFiles = 1;
         String bruteForcing;
 
-        while (index3 < alphabet.size() - 1) {
-            try (FileOutputStream fos3 = new FileOutputStream("decryptedFile.txt_" + indexesOfFiles3)) {
+        while (indexesOfFiles < alphabet.size() - 1) {
+            try (FileOutputStream fos3 = new FileOutputStream("decryptedFile.txt_" + indexesOfFiles)) {
                 bruteForcing = FileManager.readFile(filepath);
-                bruteForce = Cipher.decrypt(bruteForcing, index3);
+                bruteForce = Cipher.decrypt(bruteForcing, indexesOfFiles);
                 fos3.write(bruteForce.getBytes());
             } catch (IOException e) {
                 if (Cipher.ALPHABET.equals(Cipher.ENGLISH_ALPHABET)) {
@@ -25,24 +23,23 @@ public class BruteForce {
                     System.out.println("Произошла ошибка... Попробуйте снова: ");
                 }
             }
-            indexesOfFiles3++;
-            index3++;
+            indexesOfFiles++;
         }
     }
 
     public static void decryptByBruteForceConsole(String encryptedText, List<Character> alphabet) {
-        index3 = 1;
-        while (index3 < alphabet.size() - 1) {
-            bruteForce2 = Cipher.decrypt(encryptedText, index3);
+        indexesOfFiles = 1;
+        while (indexesOfFiles < alphabet.size() - 1) {
+            bruteForce2 = Cipher.decrypt(encryptedText, indexesOfFiles);
             if (Cipher.ALPHABET.equals(Cipher.ENGLISH_ALPHABET)) {
-                System.out.println("Here is the decryption with the key " + index3);
+                System.out.println("Here is the decryption with the key " + indexesOfFiles);
             } else {
-                System.out.println("Вот расшифровка при ключе равном " + index3);
+                System.out.println("Вот расшифровка при ключе равном " + indexesOfFiles);
             }
             System.out.println(bruteForce2);
             System.out.println();
 
-            index3++;
+            indexesOfFiles++;
         }
     }
 }
